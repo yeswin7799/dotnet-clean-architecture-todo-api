0b9c3d4fb1333e9a18b855b540718ead218c0867
yeswin7799 <yeswin9977@gmail.com>
2025-12-24 12:23:27 -0500

    2nd Round of checkin

---
+++ TodoApp.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Domain.Entities;
+
+namespace TodoApp.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TaskItem> Tasks => Set<TaskItem>();
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+                
+        }
+    }
+}
---
+++ TodoApp.Infrastructure/Repositories/EfTaskRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Application.Interfaces;
+using TodoApp.Domain.Entities;
+using TodoApp.Domain.Enums;
+using TodoApp.Infrastructure.Persistence;
+
+namespace TodoApp.Infrastructure.Repositories
+{
+    public class EfTaskRepository : ITaskRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public EfTaskRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<TaskItem?> GetByIdAsync(
+            Guid id,
+            CancellationToken cancellationToken)
+        {
+            return await _dbContext.Tasks
+                .FirstOrDefaultAsync(t => t.Id == id, cancellationToken);
+        }
+
+        public async Task AddAsync(
+            TaskItem task,
+            CancellationToken cancellationToken)
+        {
+            await _dbContext.Tasks.AddAsync(task, cancellationToken);
+        }
+
+        public async Task SaveChangesAsync(
+            CancellationToken cancellationToken)
+        {
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<int> CountHighPriorityNotFinishedByDueDateAsync(
+            DateOnly dueDate,
+            CancellationToken cancellationToken)
+        {
+            return await _dbContext.Tasks.CountAsync(
+                t => t.DueDate == dueDate &&
+                     t.Priority == Priority.High &&
+                     t.Status != Domain.Enums.TaskStatus.Finished,
+                cancellationToken);
+        }
+    }
+}
