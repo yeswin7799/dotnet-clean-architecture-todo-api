5c4aa2a8aa6c3fb552173ecdd340639107626ab4
yeswin7799 <yeswin9977@gmail.com>
2025-12-24 12:33:43 -0500

    Add infrastructure services for holidays and date provider

---
+++ TodoApp.Infrastructure/Services/HolidayService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Application.Interfaces;
+
+namespace TodoApp.Infrastructure.Services
+{
+    public class HolidayService : IHolidayService
+    {
+        private readonly HashSet<DateOnly> _holidays;
+        public HolidayService()
+        {
+            _holidays = new HashSet<DateOnly>
+            {
+                new DateOnly(2026, 1, 1),
+                new DateOnly(2026, 7, 4),
+                new DateOnly(2026, 12, 25)
+            };
+        }
+
+        public bool IsHoliday(DateOnly date)
+        {
+            return _holidays.Contains(date);
+        }
+    }
+}
---
+++ TodoApp.Infrastructure/Services/SystemDateTimeProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Application.Interfaces;
+
+namespace TodoApp.Infrastructure.Services
+{
+    public class SystemDateTimeProvider : IDateTimeProvider
+    {
+        public DateOnly Today()
+        {
+            return DateOnly.FromDateTime(DateTime.Today);
+        }
+    }
+}
